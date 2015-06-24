720e8b8c613dd346b387d3a9f0bd46c2fcdb24ee
Daniel Brezina <dandas@Daniels-MacBook-Pro.local>
2015-06-24 15:28:27 +0200

    created basic viewController without layout

--- Source/Theme.swift
+++ Source/Theme.swift
@@ -19,11 +19,11 @@ extension UIImage {
 
 extension UIFont {
     
-    class func mainRegular(size : CGFloat) -> UIFont {
+    class func mainRegular(size : CGFloat) -> UIFont! {
         return UIFont(name: "Main", size: size)!
     }
     
-    class func mainLight(size : CGFloat) -> UIFont {
+    class func mainLight(size : CGFloat) -> UIFont! {
         return UIFont(name: "Main-Light", size: size)!
     }
     
@@ -48,6 +48,48 @@ extension UIColor {
         return UIColor(hex: 0x7d8d38)
     }
     
+    class func rekolaPinkColor() -> UIColor! {
+        return UIColor(hex: 0xfd349c)
+    }
+    
+    class func rekolaGreyColor() -> UIColor! {
+        return UIColor(hex: 0xececec)
+    }
+    
+}
+
+extension UIImage {
+    enum ImageIdentifier: String {
+        case LockOff = "LockOff"
+        case LockOn = "LockOn"
+        case MapOff = "MapOff"
+        case MapOn = "MapOn"
+        case ProfileOff = "ProfileOff"
+        case ProfileOn = "ProfileOn"
+    }
+    
+    convenience init!(imageIdentifier: ImageIdentifier) {
+        self.init(named: imageIdentifier.rawValue)
+    }
+    
+    enum ImagesForToggle: String {
+        case Lock = "Lock"
+        case Map = "Map"
+        case Profile = "Profile"
+    }
+    
+    /**
+    Toggle on/off on UIButton image.
+    
+    :param: name enum of names of the images.
+    
+    :returns: tuple of images with on/off postfix.
+    */
+    class func toggleImage(name: ImagesForToggle) -> (on: UIImage!, off: UIImage!) {
+        let imageOn = UIImage(named: name.rawValue + "On")
+        let imageOff = UIImage(named: name.rawValue + "Off")
+        return (imageOn!, imageOff!)
+    }
 }
 
 
@@ -56,11 +98,35 @@ extension UIColor {
 
 */
 class Theme {
-    
     class func blueButton() -> UIButton? {
-        return nil
+        let button = UIButton()
+        button.backgroundColor = UIColor.blueColor()
+        return button
     }
     
+    class func pinkButton() -> UIButton? {
+        let button = UIButton()
+        button.backgroundColor = UIColor.rekolaPinkColor()
+        return button
+    }
     
+    class func greenButton() -> UIButton? {
+        let button = UIButton()
+        button.backgroundColor = UIColor.rekolaGreenColor()
+        return button
+    }
+    
+    class func greenBorderButton() -> UIButton? {
+        let button = UIButton()
+        button.layer.borderColor = UIColor.rekolaGreenColor().CGColor
+        button.setTitleColor(UIColor.rekolaGreenColor(), forState: .Normal)
+        return button
+    }
+    
+    class func whiteButton() -> UIButton? {
+        let button = UIButton()
+        button.setTitleColor(UIColor.rekolaGreenColor(), forState: .Normal)
+        return button
+    }
 }
     
